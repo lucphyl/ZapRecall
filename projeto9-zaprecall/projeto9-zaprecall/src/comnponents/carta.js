0e7a72c1e97fa474ce5a28f43a04f41a9381f742
lucphyl <lucphyl@gmail.com>
2022-10-02 22:47:50 -0300

    botões corrigidos e status de resposta

--- projeto9-zaprecall/projeto9-zaprecall/src/comnponents/carta.js
+++ projeto9-zaprecall/projeto9-zaprecall/src/comnponents/carta.js
@@ -1,47 +1,31 @@
 import Icone from './imagem.js';
-import CartaRespondido from './cartarespondido.js';
+
 import {useState} from 'react';
 
 
-function Pergunta({pergunta, resposta, index}){
+function Pergunta({pergunta, resposta, index, ZapCard}){
 
     const [virado, virar] = useState(false);
-
-    return(
-
-        <>
-            {!virado ?(<div className='pergunta-aberta' onClick={() => virar(true)}>
-                {pergunta}
-                <Icone mostra="vira" />
-            </div>): (<Resposta resposta={resposta} index={index}/>)
-            }
+    const [final,finalizar] = useState(false);
+   
+       return(  
             
-        </>
-    )
+           <div className='pergunta-aberta' onClick={() => virar(true)}>
+               {!virado ? pergunta : resposta}
+               {!virado ? (<div onClick={() => virar(true)}><Icone mostra="vira" /></div>) : ( 
+               <div className='espaço-botoes'>
+                    <button className='erro' onClick={() => ZapCard("erro")}>Não lembrei</button>
+                    <button className='quase' onClick={() => ZapCard("quase")}>Quase não lembrei</button>
+                    <button className='zap' onClick={() => ZapCard("certo")}>Zap!</button>
+                </div>)
+                }
+               
+           </div>
+        )
 }
 
-function Resposta ({resposta, index}){
 
-    const [virado, virar] = useState(false);
-
-    return(
-        <>
-            {!virado ? ( <div className='pergunta-aberta'>
-                {resposta}
-                <div className='espaço-botoes'>
-                    <button className='erro' onClick={() => virar(true) }>Não lembrei</button>
-                    <button className='quase' onClick={() => virar(true)}>Quase não lembrei</button>
-                    <button className='zap' onClick={() => virar(true)}>Zap!</button>
-                </div>
-                
-            </div>) : (<CartaRespondido index={index} />)}
-           
-        </>
-
-    )
-}
-
-function Carta({virar, index}){
+function Carta({virar, index, ZapCard}){
     return(
     <div className='pergunta-fechada' onClick={() => virar(true)}> 
         {`pergunta ${index}`}
@@ -49,13 +33,13 @@ function Carta({virar, index}){
     </div>)
 }
 
-export default function Flashcard({index, pergunta, resposta}){
+export default function Flashcard({index, pergunta, resposta, ZapCard}){
 
     const [virado, virar] = useState(false);
 
     return(
         <>
-            {!virado ? (<Carta virar={virar} index={index}/>): (<Pergunta pergunta={pergunta} resposta={resposta} index={index}/>)}
+            {!virado ? (<Carta virar={virar} index={index}/>): (<Pergunta pergunta={pergunta} resposta={resposta} index={index} ZapCard={ZapCard}/>)}
         </>
     )
 }
--- projeto9-zaprecall/projeto9-zaprecall/src/index.js
+++ projeto9-zaprecall/projeto9-zaprecall/src/index.js
@@ -1,11 +1,11 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-
+import Footer from './comnponents/footer';
 import "./assets/css/reset.css";
 import "./assets/css/style.css";
-
 import LogoImg from './assets/img/logo.png';
-
 import Flashcard from './comnponents/carta.js';
+
 
 let cartas = [
     { pergunta: "teste", resposta: "maçã", }, 
@@ -13,9 +13,17 @@ let cartas = [
     { pergunta: "teste3", resposta: "mamão"}, 
     { pergunta: "teste4", resposta: "laranja"}
 ];
-
+let incremento = 0;
 
 function App(){
+    //const [respondido, responder] = useState([]);
+
+
+    function ZapCard(status){
+        console.log("sou feliz", status);
+        //responder([...respondido,status]);
+        incremento++;
+    }
 
     return(
         <div className='screen-container'>
@@ -24,12 +32,18 @@ function App(){
                 <h1>zap recall</h1>
             </div>
 
-            {cartas.map((carta, index) => <Flashcard key={index} index={index+1} pergunta={carta.pergunta} resposta={carta.resposta}/>)}
+            {cartas.map((carta, index) => <Flashcard 
+                key={index} 
+                index={index+1} 
+                pergunta={carta.pergunta} 
+                resposta={carta.resposta} 
+                ZapCard={ZapCard}/>)
+            }
 
 
-            <div className='footer-concluidos'>
-                <p>0/4 respondidos</p>
-            </div>
+           < Footer >
+                {`${incremento}/${cartas.length} respondidos`}
+           </Footer>
         </div>
     )
 }
